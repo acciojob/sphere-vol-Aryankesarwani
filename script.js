0edf79fa6b3ae6eddb853261acc0d84bb5cff40f
Aryan Kesarwani <93967099+Aryankesarwani@users.noreply.github.com>
2025-10-16 19:36:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-function volume_sphere() {
-    //Write your code here
-	let r = document.getElementById("radius").value;
-	let v = document.getElementById("volume");
-	r = parseFloat(r);
-	if (!isNaN(r) && r >= 0) {
-		let volume = (4/3) * Math.PI * Math.pow(r,3);
-		v.value = volume.toFixed(4);
-	}
-	else v.value = "NAN";
-  return v.value;
-} 
+function volume_sphere(event) {
+    event.preventDefault();
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    let r = parseFloat(document.getElementById("radius").value);
+    let v = document.getElementById("volume");
+
+    if (!isNaN(r) && r >= 0) {
+        let volume = (4 / 3) * Math.PI * Math.pow(r, 3);
+        v.value = volume.toFixed(4);
+    } else {
+        v.value = "NaN";
+    }
+}
+
+window.onload = function() {
+    document.getElementById("MyForm").onsubmit = volume_sphere;
+};
